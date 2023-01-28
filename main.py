ba3fded309fc7928301230cbf4bb8c64f3ab2928
Sanyavas <vasilinaoleksandr@gmail.com>
2023-01-28 13:34:46 +0200

    add start main

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# """CHATBOT"""
-#
-#
-# """
-# Головна функція
-# """
-#
-#
-# def main():
-#     print("Hello World!")
-#
-#
-# if __name__ == "__main__":
-#     main()
+"""CHATBOT"""
+
+from addressbook import *
+
+"""
+Головна функція
+"""
+
+
+def main():
+    user_input = input("Main menu. Enter command >>>")
+    if user_input == "addressbook":
+        main_addressbook()
+    else:
+        print(f"I don't understand you!!")
+
+
+if __name__ == "__main__":
+    main()
